b0ae3b6687734c6e989bc5d3eb5ba1f6f828a7d8
AashayT <tinaikar.aashay@gmail.com>
2018-11-05 22:32:03 +0100

    Implemented 2nd level wavelet transform

--- MRIprocessing.m
+++ MRIprocessing.m
@@ -3,8 +3,8 @@ clear All,
 clc
 
 %STEP1: Loading the MRI image
-%img = imread('mri_heart_1.jpg');
-img = imread('Phantom1.png');
+img = imread('mri_heart_1.jpg');
+%img = imread('Phantom1.png');
 img = rgb2gray(img);
 figure(1)
 imshow(img)
@@ -43,10 +43,10 @@ imshow(real(img_kspace))
     
     %US Scheme 4: Spiral undersampling
     dtheta = 5;
-    W = 1/5000;
+    W = 1/20000;
     theta = (2*pi/360).*[0:dtheta:floor(400/W)];
-    spiral_x = (((W*360/2*pi).*theta).*cos(theta))';
-    spiral_y = (((W*360/2*pi).*theta).*sin(theta))';
+    spiral_x = ((W*360/2*pi).*(1.*theta).*cos(theta))';
+    spiral_y = ((W*360/2*pi).*(1.*theta).*sin(theta))';
     spiral_points = floor([spiral_x spiral_y]);
     und_samp_temp = img_kspace;
     for i=1:size(spiral_points,1)
@@ -74,23 +74,64 @@ title('50% undersampled')
 % figure(6), histogram(img_recov)
 % 
 % %STEP6: Taking wavelet transform of input MRI image
-[cA, cH, cV, cD] = dwt2(img_recov,'db1');
-figure(7), imshow(cA)
-figure(8), imshow(cH)
-figure(9), imshow(cV)
-figure(10), imshow(cD)
+
+% For 1 level decomposition
+% [cA, cH, cV, cD] = dwt2(img_recov,'db1');
+% figure(7), imshow(cA)
+% figure(8), imshow(cH)
+% figure(9), imshow(cV)
+% figure(10), imshow(cD)
+
+% For N level decomposition
+N = 2;
+[C,S]= wavedec2(img,N,'db1');
+
+% In the above [C] =  vector with all the coefficients of transform
+% and [S] = contain structure of each transformed image
+
+% Restructuring the coeff matrix [C] 
+i = 1;
+j = prod(S(1,:));
+cA2 = reshape(C(i:j),S(1,:));
+
+i = j+1;
+j = i + prod(S(2,:)) - 1;
+cH2 = reshape(C(i:j),S(2,:));
+
+i = j+1; 
+j = i + prod(S(2,:)) - 1;
+cV2 = reshape(C(i:j),S(2,:));
+
+i = j+1; 
+j = i + prod(S(2,:)) - 1;
+cD2 = reshape(C(i:j),S(2,:));
+
+i = j+1; 
+j = i + prod(S(3,:)) - 1;
+cH1 = reshape(C(i:j),S(3,:));
+
+i = j+1; 
+j = i + prod(S(3,:)) - 1;
+cV1 = reshape(C(i:j),S(3,:));
+
+i = j+1; 
+j = i + prod(S(3,:)) - 1;
+cD1 = reshape(C(i:j),S(3,:));
 % %     %See how the wavelet domain representation is different than fourier
 % %     %domain represetation. MRI data is considerably sparse in wavelet domain.
 % %     
-% 
+
+% Displaying these images 
+figure(11), imshow([[cA2,cH2(:,1:end-1); cV2(1:end-1,:),cD2(1:end-1,1:end-1)],cH1;cV1,cD1],'Colormap',pink)
+
 % %Thresholding the transform domain
-t1= im2bw(uint8(real(cD)),0.3);
-t2= im2bw(uint8(imag(cD)),0.3);
-t = t1 + t2.*1i;
+% t1= im2bw(uint8(real(cD)),0.3);
+% t2= im2bw(uint8(imag(cD)),0.3);
+% t = t1 + t2.*1i;
 % 
 % % %STEP7: Take inverse wavelet transform to recover the original image
-img_wav_recon = uint8(idwt2(cA, cH, cV, t, 'db1'));
-figure(11), imshow(img_wav_recon)
+% img_wav_recon = uint8(idwt2(cA, cH, cV, t, 'db1'));
+% figure(11), imshow(img_wav_recon)
 
 
 
