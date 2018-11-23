fa4001d5044331ad57806cacccd1a473cf2b39d0
AashayT <tinaikar.aashay@gmail.com>
2018-11-23 15:12:10 +0100

    Code cleared and commented

--- CSreconstruction.m
+++ CSreconstruction.m
@@ -1,35 +1,55 @@
 % Energy function for compressed sensing reconstruction
-% E = || F(desired_img) - y ||.^2 + lambda||grad(desired_img)||
+% E = || F(desired_img) - y ||.^2 + lambda||phi(desired_img)||
+% Type of function 'phi' is chosen from the specific application. Usually a
+% function phi is chosen such that it transforms the image into a sparse
+% domain.
+% For angiography images: Gradient
+%     Normal MRI images : Wavelet 
+%
+% Currently only Total variation is considered as the sparsifying transform
+% TODO: Addition of wavelet into the reconstruction equation
+%
+% function [new_filtered_image] = CSreconstruction(und_samp_kspace)
+% 
+% where     new_filtered_image = a list with images stored at interval of 5
+%           und_same_kspace = k-space undersampled data
 
 function [new_filtered_image] = CSreconstruction(und_samp_kspace)
-    
     %close all
     
+    % Parameter initialization
+    % lambda = regularizer weighting between consistency and smoothness
+    %           term
+    % alpha = step size for gradient decent algorithm
     lambda = 1;
     eps = 1e-6;
     alpha = 0.1;
-    %und_samp_kspace = double(und_samp_kspace);
-    %und_samp_kspace = und_samp_kspace./255;
     [ny nx] = size(und_samp_kspace);
     [Grad,Dx,Dy] = spmat_gradient2d(ny,nx,1);
     
-    % Initializing the filtered image
+    %For testing on noisy image in k-space representation
     acquired_image = real(ifft2(ifftshift(und_samp_kspace)));
     acquired_image = double(uint8(acquired_image));
+    
+    %For testing on noisy image in pixel representation
     %acquired_image = double(uint8(real(und_samp_kspace)));
     %figure(1), imagesc(acquired_image), colormap gray
     
-    % Generating copy for beginning of optimization
-    filtered_image = acquired_image;
+    % Initializing the filtered image
+    % Here we have considered the 1st filtered image = acquired image.
+    % However this can be initialized differently 
+    
+    filtered_image = 1*acquired_image;
     %figure(), imagesc(filtered_image), colormap gray
-
-    for i=1:1000
+    
+    %Iterations are hard coded, can be provided as function argument
+    for i=1:100
     %% Calculating the energy functionals
 
         % Using L2-Norm for calculating data fidelity term. Considering that the
         % errors between actual and acquired pixel values follow a Gaussian
         % distribution
-        E1 = 0*((filtered_image - acquired_image).^2);
+        E1 = 1*((filtered_image - acquired_image).^2);
         % Initially both images are same. Therefore 
         % max(max(E1))== min(min(E1)) = 0
         
@@ -44,17 +64,19 @@ function [new_filtered_image] = CSreconstruction(und_samp_kspace)
         % sudden discontinuities. L1-Norm is an edge preverving regularizer
         [grad_filtered_x, grad_filtered_y] = gradient(filtered_image);
         Norm_grad = sqrt(grad_filtered_x.^2 + grad_filtered_y.^2) + eps;
-        %E2 = abs(Norm_grad);
-        E2 = Norm_grad.^2;
+        E2 = abs(Norm_grad);
+        %E2 = Norm_grad.^2;
         %figure(), imagesc(Norm_grad)
 
         TE = sum(sum(0.01*E1 + lambda*E2));
         figure(2), plot(i,TE, 'o'), hold on
-
+        
         %% Calculation of Getaux derivative or directional derivative of T.E.
-        L1 = 0*2*abs(filtered_image - acquired_image);
+        L1 = 1*2*abs(filtered_image - acquired_image);
 
         [X, Y] = meshgrid(1:size(filtered_image,2), size(filtered_image,1):1);
+        
+        %Whether to scale it or not, needs to be investigated further
         %X = X/size(filtered_image,2);
         %Y = Y/size(filtered_image,1);
         L2 = divergence(X, Y, (grad_filtered_x./(Norm_grad)), (grad_filtered_y./(Norm_grad)));
@@ -62,7 +84,7 @@ function [new_filtered_image] = CSreconstruction(und_samp_kspace)
 
         dL_du = 0.01*L1 - lambda*L2;
         
-        %Witing the solution in form of a linear equation AX=B. 
+        %Writing the solution in form of a linear equation AX=B. 
         %Following formulation may not be perfect. Please verify
         %sp_dL_du = spdiags(dL_du(:),0,(size(dL_du,1)*size(dL_du,2)),(size(dL_du,1)*size(dL_du,2)));
         %A = pcg(sp_dL_du, acquired_image(:));
@@ -73,10 +95,12 @@ function [new_filtered_image] = CSreconstruction(und_samp_kspace)
         filtered_image = filtered_image - alpha*(dL_du);
         %figure(3), imagesc(filtered_image), colormap gray, hold on;
         
+        %Saving the denoised/reconstructed image in a folder. 
+        %Images are saved at interval of 5
         if mod(i,5)==0
             Norm_gragient{i/5} = Norm_grad;
             new_filtered_image{i/5} = filtered_image;
-            imwrite(uint8(new_filtered_image{i/5}),['./Denoised_phantom_full//I' num2str(i)], 'png');
+            imwrite(uint8(new_filtered_image{i/5}),['./temp/I' num2str(i)], 'png');
         end
     end
 end
--- MRIprocessing.m
+++ MRIprocessing.m
@@ -4,8 +4,10 @@ clear All,
 clc
 
 %STEP1: Loading the MRI image
-%img = imread('mri_heart_1.jpg');
-img = imread('Phantom1.png');
+%Select either the mr-heart image or created test image.
+
+img = imread('mri_heart_1.jpg');
+%img = imread('Phantom1.png');
 img = rgb2gray(img);
 figure(1)
 imshow(img)
@@ -16,20 +18,17 @@ figure(2)
 imshow(real(img_kspace))
 
 %STEP3: Undersampling the k-space. 
-%Rearrange the fft values before undersampling
+%FFt values are rearranged before undersampling using fftshift
 
-    %US Scheme 1: We take alternate lines
+    %US Scheme 1: We take alternate lines (in X or Y directions)
     %und_samp_kspace = img_kspace;
     %und_samp_kspace(1:2:end,1:2:end)= 0;
-    %figure(3)
-    %imshow(real(und_samp_kspace))
-    %title('50% undersampled')
 
     %US Scheme 2: Implementing high/low pass filter
 %     undersampling_mask_LPF = ones(size(img_kspace,1), size(img_kspace,2));
 %     undersampling_mask_LPF(140:284, 220:366)= 0;
 %     und_samp_kspace = img_kspace - img_kspace.*undersampling_mask_LPF;
-    
+
     %US Scheme 3: Random undersampling
 %     rand_x_cord = randperm((size(img_kspace,1)*size(img_kspace,2)), 8000);
 %     rand_x_cord = mod(rand_x_cord, size(img_kspace,1)) + 1;
@@ -62,18 +61,20 @@ imshow(real(img_kspace))
     
     figure(3)
     imshow(real(und_samp_kspace))
-    title('percent undersampling')
+    title([num2str(100*factorUS) ' percent undersampling'])
     
-%STEP4: Acquiring the image back
+%STEP4: Acquiring the image back [complete k-space sampling]
 img_recov = ifft2(ifftshift(und_samp_kspace));
 figure(4)
 imshow(uint8(img_recov))
-title('10% undersampled')
+title([num2str(100*factorUS) ' percent undersampled'])
 
-% %STEP5: Comparing images by taking histogram
-% figure(5), histogram(img)
-% figure(6), histogram(img_recov)
-% 
+%STEP5: Using CS/variational approach for denoising and reconstruction 
+%Images will be saved in the folder named 'temp'.
+%Create a new folder named 'temp' before running the following command.
+%Open the function file for more details
+CSreconstruction(und_samp_kspace);
+
 % %STEP6: Taking wavelet transform of input MRI image
 
 % For 1 level decomposition
@@ -83,56 +84,56 @@ title('10% undersampled')
 % figure(9), imshow(cV)
 % figure(10), imshow(cD)
 
-% For N level decomposition
-N = 2;
-[C,S]= wavedec2(img,N,'db1');
-
-% In the above [C] =  vector with all the coefficients of transform
-% and [S] = contain structure of each transformed image
-
-% Restructuring the coeff matrix [C] 
-i = 1;
-j = prod(S(1,:));
-cA2 = reshape(C(i:j),S(1,:));
-
-i = j+1;
-j = i + prod(S(2,:)) - 1;
-cH2 = reshape(C(i:j),S(2,:));
-
-i = j+1; 
-j = i + prod(S(2,:)) - 1;
-cV2 = reshape(C(i:j),S(2,:));
-
-i = j+1; 
-j = i + prod(S(2,:)) - 1;
-cD2 = reshape(C(i:j),S(2,:));
-
-i = j+1; 
-j = i + prod(S(3,:)) - 1;
-cH1 = reshape(C(i:j),S(3,:));
-
-i = j+1; 
-j = i + prod(S(3,:)) - 1;
-cV1 = reshape(C(i:j),S(3,:));
-
-i = j+1; 
-j = i + prod(S(3,:)) - 1;
-cD1 = reshape(C(i:j),S(3,:));
-% %     %See how the wavelet domain representation is different than fourier
-% %     %domain represetation. MRI data is considerably sparse in wavelet domain.
-% %     
-
-% Displaying these images 
-figure(11), imshow([[cA2,cH2(:,1:end-1); cV2(1:end-1,:),cD2(1:end-1,1:end-1)],cH1;cV1,cD1],'Colormap',pink)
-
-% %Thresholding the transform domain
-% t1= im2bw(uint8(real(cD)),0.3);
-% t2= im2bw(uint8(imag(cD)),0.3);
-% t = t1 + t2.*1i;
+% % For N level decomposition
+% N = 2;
+% [C,S]= wavedec2(img,N,'db1');
 % 
-% % %STEP7: Take inverse wavelet transform to recover the original image
-% img_wav_recon = uint8(idwt2(cA, cH, cV, t, 'db1'));
-% figure(11), imshow(img_wav_recon)
+% % In the above [C] =  vector with all the coefficients of transform
+% % and [S] = contain structure of each transformed image
+% 
+% % Restructuring the coeff matrix [C] 
+% i = 1;
+% j = prod(S(1,:));
+% cA2 = reshape(C(i:j),S(1,:));
+% 
+% i = j+1;
+% j = i + prod(S(2,:)) - 1;
+% cH2 = reshape(C(i:j),S(2,:));
+% 
+% i = j+1; 
+% j = i + prod(S(2,:)) - 1;
+% cV2 = reshape(C(i:j),S(2,:));
+% 
+% i = j+1; 
+% j = i + prod(S(2,:)) - 1;
+% cD2 = reshape(C(i:j),S(2,:));
+% 
+% i = j+1; 
+% j = i + prod(S(3,:)) - 1;
+% cH1 = reshape(C(i:j),S(3,:));
+% 
+% i = j+1; 
+% j = i + prod(S(3,:)) - 1;
+% cV1 = reshape(C(i:j),S(3,:));
+% 
+% i = j+1; 
+% j = i + prod(S(3,:)) - 1;
+% cD1 = reshape(C(i:j),S(3,:));
+% % %     %See how the wavelet domain representation is different than fourier
+% % %     %domain represetation. MRI data is considerably sparse in wavelet domain.
+% % %     
+% 
+% % Displaying these images 
+% figure(11), imshow([[cA2,cH2(:,1:end-1); cV2(1:end-1,:),cD2(1:end-1,1:end-1)],cH1;cV1,cD1],'Colormap',pink)
+% 
+% % %Thresholding the transform domain
+% % t1= im2bw(uint8(real(cD)),0.3);
+% % t2= im2bw(uint8(imag(cD)),0.3);
+% % t = t1 + t2.*1i;
+% % 
+% % % %STEP7: Take inverse wavelet transform to recover the original image
+% % img_wav_recon = uint8(idwt2(cA, cH, cV, t, 'db1'));
+% % figure(11), imshow(img_wav_recon)
 
 
 
